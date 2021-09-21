cbe769e649b9ff175473e0403bcbb7ef3bed2100
S317 <seifenan@gmail.com>
2021-09-20 23:03:07 -0400

    updated JS & HTML

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,46 @@
-var apiKey = "50ba35b451e58d2b8a2ec99ebcab0d37";
 
-var userInput = document.querySelector("#userInput");
-
-var searchBtn = document.querySelector("#searchBtn");
-
-var userCity = userInput.value.trim();
-
-function getWeather(userCity) {
-  fetch(
-  "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + apiKey
-)
-  .then(function(response) {
-    return response.json();
-  })
-  
-  console.log(userInput);
-
-  // .then(function(data) {
-  //   console.log(data);
-  // });
+var currentDate = (moment().format("dddd, MMMM D, YYYY"));
 
 
-}
 
-  
-  
+var getWeather = {
+  apiKey : "50ba35b451e58d2b8a2ec99ebcab0d37",
+  fetchWeather : function(city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/forecast?q=" 
+        + city
+        + "&units=imperial&appid="
+        + this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather : function(data) {
+    var name = data.city.name;
+    var icon = data.list[0].weather[0].icon;
+    var description = data.list[0].weather[0].description;
+    var temp = data.list[0].main.temp;
+    var humidity = data.list[0].main.humidity;
+    var speed = data.list[0].wind.speed;
+
+    console.log(name,description,icon,temp,humidity,speed);
+
+    
+
+    document.querySelector(".name").innerHTML = name + " (" + currentDate + ")";
+    document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerHTML = "Weather Conditions: " + description;
+    document.querySelector(".temp").innerHTML = "Temperature: " + temp + "° F";
+    document.querySelector(".humidity").innerHTML = "Humidity Level: " + humidity + " %";
+    document.querySelector(".speed").innerHTML = "Wind Speed: " + speed + " Miles per Hour"; 
 
 
-//---Event Listeners---//
+  }
+};
 
-searchBtn.addEventListener("click", getWeather);
+
+
+
+
+
+
